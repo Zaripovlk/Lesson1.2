fa042fb4898e0b48587ab93730acc623dd18e7a9
Zaripov Linar <Zaripovlk@gmail.com>
2023-12-21 21:25:33 +0300

    lesso1.2:Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+//    является ли первое число квадратом второго.
+//Console.Write"Enter a number 1:")
+//string a1 = Console.ReadLine()!;
+//Console.Write"Enter a number 2:")
+//string a2 = Console.ReadLine()!;
+//int b1 = Convert.ToInt32(a1);
+//int b2 = Convert.ToInt32(a2);
+//if (b2*b2==b1)
+//    Console.WriteLine("Yes");
+//else 
+ //   Console.WriteLine("No");
+
+    Console.Write("Enter a number 1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter a number 2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+
+if (b2 * b2 == b1) 
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
