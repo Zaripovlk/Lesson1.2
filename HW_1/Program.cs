e5fc17cdf31ea3d46aa4184b454b83fd78754b9c
Zaripov Linar <Zaripovlk@gmail.com>
2023-12-25 15:21:33 +0300

    added 1 task hw

---
+++ HW_1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter a number 1: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.Write("Enter a number 2: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+//static void CompareNumbers(int firstNumber, int secondNumber);
+{
+    // Введите свое решение ниже
+    if (firstNumber == secondNumber){
+    Console.WriteLine("Введенные числа равны `" + firstNumber + "`");
+ } else {
+    if (firstNumber < secondNumber) {
+        Console.WriteLine("Первое число `" + firstNumber + "` меньше чем второе число `" + secondNumber + "` ");
+    } else {
+        Console.WriteLine("Первое число `" + firstNumber + "` больше чем второе число `" + secondNumber + "` ");
+    }
+    }
+
+    
+
+    
+}
